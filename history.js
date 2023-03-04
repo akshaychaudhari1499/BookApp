f35f385220b22972f38cb676e816c72ad20be7ca
Akshay Chaudhari <akshaychaudhari1499@gmail.com>
2023-03-04 19:18:42 +0530

    Completed All the features

--- history.js
+++ history.js
@@ -3,14 +3,16 @@ let clear=document.getElementById('clear');
 console.log(clear)
 let booksSearched=JSON.parse(localStorage.getItem('value'))
 console.log(typeof(booksSearched));
-
+let images=document.getElementById('box-search')
 let home=document.createElement('button');
-   
+home.classList.add('btn');
+home.classList.add('homebtn');
 clear.addEventListener('click',(e)=>{
     console.log('clear clicked')
     e.preventDefault();
     localStorage.clear();
-    searchedContent.innerHTML=`<h1>All previous searches Deleted</h1>`
+    searchedContent.innerHTML=`<h1 class='heading'>All previous searches Deleted</h1>`
+    images.innerHTML='';
 home.innerText='Go to home';
 searchedContent.appendChild(home);
 home.addEventListener('click',(e)=>{
@@ -18,19 +20,29 @@ home.addEventListener('click',(e)=>{
     window.location.href= 'index.html';
 
 })
-    
+console.log(images)
 })
 for(let i=0;i<= booksSearched.length ;i++){
 
 let book=document.createElement('div');
-book.innerHTML=`<div class="searchedContent" id="box-seach">
-<h3>${i+1}.</h3> <h3>${booksSearched[i].value}</h3> <h3>Searched on : ${booksSearched[i].date}</h3>
-
-</div>`
+book.innerHTML=`<div class='rectangle'>
+<h3>${i+1}.</h3> <h3>${booksSearched[i].value}</h3> <h3>Searched on : ${booksSearched[i].date}</h3></div>`
 searchedContent.appendChild(book)
 console.log(booksSearched[i])
+book.addEventListener('click',(e)=>{
+console.log(e.target);
 
+let historybooks=booksSearched[i].info;
 
-
-} 
-
+historybooks.map(book=> {    
+    const card1 = document.createElement('div');
+    card1.id = "history-box";                
+    card1.innerHTML =`<div id='card'><img src="${book.volumeInfo.imageLinks.thumbnail}"><h2>Title:${book.volumeInfo.title}</h2><h2>Author : ${book.volumeInfo.authors}</h2></div>`;
+    images.appendChild(card1)                     
+          
+}
+    
+)
+   
+}
+)}
--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let button=document.getElementById('submit');
 let result=document.getElementById('result');
 let url="https://www.googleapis.com/books/v1/volumes?q="
 const title=document.createElement('div');
+title.id='interesting'
 const history= document.createElement('div');
 let titleCont=document.getElementById('titleContainer');
 const searchedItems=[];
@@ -15,20 +16,29 @@ function(e){
             console.log(result)        
             let books=data.items;
             console.log(books);  
-            let date=new Date();      
+           let currentDate = new Date();
+           let date;
+           if(currentDate.getHours()>12){
+           date =currentDate.getDate()+ "/" + currentDate.getMonth() + 1 + "/" + currentDate.getFullYear()+ ' at ' +Number(currentDate.getHours()-12) + ":" + currentDate.getMinutes() + " PM";
+           }else{
+            date=currentDate.getDate()+ "/" + currentDate.getMonth() + 1 + "/" + currentDate.getFullYear()+ ' at ' +currentDate.getHours() + " : " + currentDate.getMinutes() + " AM";
+           }
           searchedItems.push({'value':searchbar.value,
-        'date':date
+        'date':date,
+        'info':books
         });        
             localStorage.setItem('value',JSON.stringify(searchedItems));       
-            title.innerHTML=`<h1>Books results for ${searchbar.value}</h1>`
+            title.innerHTML=`<h1 class='heading' >Books results for ${searchbar.value}</h1>`
             titleCont.appendChild(title)
-            history.innerHTML=`<button id='view'>View History</button>`
+            history.innerHTML=`<button class=' btn' id='view'>View History</button>`
             titleCont.appendChild(history);            
             console.log(searchedItems)
             books.map(book=>{
                 const textnode = document.createElement('div');
                 textnode.id = "box";                
-                textnode.innerHTML =`<div id='card'><img src="${book.volumeInfo.imageLinks.thumbnail}"><h2>Title:${book.volumeInfo.title}</h2><h2>Author : ${book.volumeInfo.authors}</h2></div>`;
+                textnode.innerHTML =`<div id='card'><div id='img'><img src="${book.volumeInfo.imageLinks.thumbnail}"></div><h2>Title:${book.volumeInfo.title}</h2><h2>Author : ${book.volumeInfo.authors}</h2>
+                <h2>Page Count : ${book.volumeInfo.pageCount}</h2><h2>Publisher:${book.volumeInfo.publisher}</h2>
+                </div>`;
                         result.appendChild(textnode)                     
                         console.log(data)
             })
